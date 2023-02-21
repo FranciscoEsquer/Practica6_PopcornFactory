c4b4b8033809988478510cc395d2812128115f72
FranciscoEsquer <franciscod.esquer@potros.itson.edu.mx>
2023-02-20 21:17:59 -0700

    popcorn - seats

--- app/src/main/java/esquer/francisco/popcornfactory/MovieDetail.kt
+++ app/src/main/java/esquer/francisco/popcornfactory/MovieDetail.kt
@@ -1,12 +1,28 @@
 package esquer.francisco.popcornfactory
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+
+
 
 
 class MovieDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail)
-        }
+
+        var image = intent.getIntExtra("image", 0)
+        var titulo = intent.getStringExtra("titulo")
+        var header = intent.getIntExtra("header", 0)
+        var sinopsis = intent.getStringExtra("sinopsis")
+
+        var detailImage = findViewById<ImageView>(R.id.detail_view)
+        var detailTitle = findViewById<TextView>(R.id.detail_title)
+        var detailDescription = findViewById<TextView>(R.id.detail_desc)
+
+        detailImage.setImageResource(image)
+        detailTitle.text = titulo
+        detailDescription.text = sinopsis
     }
+}
